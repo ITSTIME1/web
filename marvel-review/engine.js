c32ed1412682b738363d7d52ef2b4be3fa949662
itstime <itstime0809@gmail.com>
2023-02-02 18:38:50 +0900

    engine start

---
+++ marvel-review/engine.js
@@ -0,0 +1,32 @@
+// 이미지를 클릭했을 경우 랜덤으로 이미지가 변경되면서
+// 설명이랑 이름이 바뀌는거
+// 추가적으로 keyframe 을 사용해서 효과를 넣기
+// 뭔가 허접한가..?
+
+const character = [
+    {
+        id: 1,
+        name: "Thor",
+        description: "Thor Odinson, more commonly known as Thor, and sometimes by his title as the God of Thunder.",
+        power_rate: "4 / 5.0"
+    },
+    {
+        id: 2,
+        name: "BlackWidow",
+        description: "Black Widow is a fictional character appearing in American comic books published by Marvel Comics. Created by editor and plotter Stan Lee, scripter Don Rico, and artist Don Heck, the character debuted in Tales of Suspense.",
+        power_rate: "3.5 / 5.0"
+    },
+    {
+        id: 3,
+        name: "Iron man",
+        description: "Iron Man is a superhero appearing in American comic books published by Marvel Comics. The character was co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby.",
+        power_rate: "4.5 / 5.0"
+    },
+    {
+        id: 4,
+        name: "Hulk",
+        description: "The Hulk is a superhero appearing in American comic books published by Marvel Comics. Created by writer Stan Lee and artist Jack Kirby.",
+        power_rate: "5.0 / 5.0"
+    }
+];
+
