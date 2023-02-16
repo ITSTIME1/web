c3ba72724e0ae15478f1f841b0dfc7e2dc5720b8
ITSTIME <itstime0809@gmail.com>
2023-02-16 16:28:33 +0900

    good

--- koGPT/server/app.js
+++ koGPT/server/app.js
@@ -7,21 +7,22 @@ var app = express()
 app.use(cors());
 
 
+
 // rest api
 const REST_API_KEY = '4c62bd862e78e1365f336d5a51e12cd3'
 const urlString = 'https://api.kakaobrain.com/v1/inference/kogpt/generation';
-const max_tokens = 32;
+const max_tokens = 100;
 const temperature = 1.0;
-const top_p = 1.0;
-const n = 1;
-
+const top_p = 0.;
+const n = 3;
+const prompt = "인천~뉴욕 구간의 프레스티지석을 보너스 항공권으로 구매하려면 종전에는 편도 6만 2천500마일이 필요했지마느 개편안이 시행되면 9만 마일이 필요합니다. 한줄 요약: "
 var options = {
     host: "api.kakaobrain.com",
     uri: urlString,
     method: 'POST',
     // body 따로 받아야됨.
     body: {
-        prompt: "조선시대 14대 왕은 어떤 사람이고, 무엇을 했을까",
+        prompt: prompt,
         max_tokens: max_tokens,
         temperature: temperature,
         top_p: top_p,
@@ -33,8 +34,6 @@ var options = {
         'Content-Type': 'application/json',
     }
 };
-
-
 
 
 // nodemon installed
@@ -49,13 +48,14 @@ app.listen(3000, () => {
 // get front
 app.get('/2000', (req, res) => {
     res.sendFile(__dirname + "/front/index.html");
+    request.post(options, (error, response, body)=>{
+        console.log(body);
+    });
 });
 
 // request model continude....
 
 
 
-// request.post(options, (error, response, body)=>{
-//     console.log(body);
-// });
+
 
